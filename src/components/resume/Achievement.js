93999751a0d0ce1fe8e47a3769d518a3ac62609f
aldhussein <145017137+aldhussein@users.noreply.github.com>
2024-12-31 17:47:57 +0300

    Update Achievement.js

--- src/components/resume/Achievement.js
+++ src/components/resume/Achievement.js
@@ -7,9 +7,9 @@ const Achievement = () => {
     <motion.div
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { duration: 0.5 } }}
-      className="py-12 font-titleFont flex gap-20"
+      className="py-12 font-titleFont flex gap-20 max-sm:flex-col"
     >
-      <div className="flex sm:flex-col">
+      <div>
         <div className="py-12 font-titleFont flex flex-col gap-4">
           <p className="text-sm text-designColor tracking-[4px]">2020 - 2024</p>
           <h2 className="text-4xl font-bold">Freela Experience</h2>
