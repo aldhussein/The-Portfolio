e3c52b5c5f613d3db87cf4bfd7bae8470a6de474
aldhussein <145017137+aldhussein@users.noreply.github.com>
2025-01-02 14:44:25 +0300

    Update Banner.js
    adding div

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -7,9 +7,10 @@ const Banner = () => {
       id="home"
       className="w-full pt-10 pb-20 flex flex-col xl:gap-0 lgl:flex-row items-center border-b-[1px] font-titleFont border-b-black"
     >
+      <div className="flex fex-col">
       <LeftBanner />
      <RightBanner />
-        
+      <div/>
     </section>
   );
 }
