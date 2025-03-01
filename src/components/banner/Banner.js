9eaff847f01646f1b00187f7a7dea60596d0a4a6
aldhussein <145017137+aldhussein@users.noreply.github.com>
2025-03-01 08:58:41 +0300

    Update Banner.js

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
   return (
     <section
       id="home"
-      className="w-full pt-10 pb-20 flex xl:gap-0 items-center border-b-[1px] font-titleFont border-b-black"
+      className="w-full pt-10 pb-20 flex sm:flex-col xl:gap-0 items-center border-b-[1px] font-titleFont border-b-black"
     >
      
       <LeftBanner />
