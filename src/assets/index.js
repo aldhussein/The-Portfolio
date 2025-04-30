c83c2996abc6c1dde309a45a3d269955790d6956
aldhussein <145017137+aldhussein@users.noreply.github.com>
2025-04-30 19:04:02 +0300

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@ import logo from "./images/mylogo.png";
 import bannerImg from "./images/profile.png";
 import Resume from "./images/MyResume (Up).pdf";
 import projectOne from "./images/projects/restaurant.jpg";
-import projectTwo from "./images/projects/sneakers.jpeg";
+import projectTwo from "./images/projects/todo.jpeg";
 import projectThree from "./images/projects/boostore.jpg";
 import projectFour from "./images/projects/digital.jpeg";
 import projectFive from "./images/projects/elearning.jpg";
